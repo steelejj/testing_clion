261ba3826b08724e4cd913c935d9c4cbb389b852
James Steele <steelejj@bc.edu>
2019-09-02 18:36:04 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/testing_clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testing_clion.dir/main.c.o"
+  "testing_clion.pdb"
+  "testing_clion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testing_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Testing new IDE and deployment.!\n");
+    return 0;
+}
